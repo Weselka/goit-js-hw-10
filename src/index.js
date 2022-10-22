386a8c0043932e9fe222a0957bc39316a1e724d9
Weselka <weselyna.shvets@gmail.com>
2022-10-22 14:59:13 +0300

    add classes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
@@ -22,7 +23,7 @@ function onInputSearch(event) {
   console.log(inputSearch);
 
   if (inputSearch.length === 0) {
-    return alert('Enter country');
+    return Notify.failure('Enter country');
   }
   
   fetchCountries(inputSearch).then(country => {
@@ -32,10 +33,9 @@ function onInputSearch(event) {
     } else if (country.length >= 2 && country.length <= 10) {
       renderCountryList(country);
     } else {
-      alert('Too many countries')
+      Notify.info('Too many matches found. Please enter a more specific name.');
     }
   }).catch(onFetchError);
-  //.finally(() => inputSearch.reset());
 }
 
 function clearMarkup () {
@@ -43,29 +43,31 @@ function clearMarkup () {
   refs.countryList.innerHTML = '';
 }
 
-function renderCountryItem([{ name, capital, population, flags, languages }]) {
-  const markupCard = `<div>
-  <img src="${flags.svg}" alt="flags" width=100>
-      <h2>Country: ${name.official}</h2>
-      <p>Capital: ${capital}</p>
-      <p>Population: ${population}</p>      
-      <p>Languages: ${Object.values(languages).join(', ')}</p></div>`;
-  refs.container.innerHTML = markupCard;
-}
-
 function renderCountryList(country) {
   console.log(country);
   const markup = country
     .map(
       ({ name, flags }) =>
-        `<li>
-      <h2>Country:${name.common}</h2>
-      <img src="${flags.svg}" alt="flags" width=30>`
+        `<li class='country-item'>
+      <img class='flag' src="${flags.svg}" alt="flags" width=30>
+      <h2 class='country-title'>${name.common}</h2>`
     )
     .join('');
   refs.countryList.innerHTML = markup;
 }
 
+function renderCountryItem([{ name, capital, population, flags, languages }]) {
+  const markupCard = `<div class='country-box'>
+  <img class='flag' src="${flags.svg}" alt="flags" width=30>
+      <h2> ${name.official}</h2></div>
+      <div><p class='country-text'><span class='country-span'>Capital: </span>${capital}</p>
+      <p class='country-text'><span class='country-span'>Population: </span>${population}</p>      
+      <p class='country-text'><span class='country-span'>Languages: </span>${Object.values(
+        languages
+      ).join(', ')}</p></div>`;
+  refs.container.innerHTML = markupCard;
+}
+
 function onFetchError(error) {
-  alert('Oops, there is no country with that name');
+   Notify.failure('Oops, there is no country with that name');
 }
